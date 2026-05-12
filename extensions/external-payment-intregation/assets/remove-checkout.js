2813e51fa105a2a4c5cfc9863338389388e32db4
MdZillurRahman <mzisanr@gmail.com>
2026-05-12 17:35:10 +0600

    updated embed block to remove checkout and other element

--- extensions/external-payment-intregation/assets/remove-checkout.js
+++ extensions/external-payment-intregation/assets/remove-checkout.js
@@ -2,6 +2,16 @@ if (document.querySelector("#main-cart-footer")) {
   document.querySelector("#main-cart-footer").remove();
 }
 
-if (document.querySelectorAll("[name=checkout]").length > 0) {
+if (document.querySelectorAll("[name=checkout], [data-shopify=payment-button]").length > 0) {
   document.querySelectorAll("[name=checkout]").forEach(el => el.remove());
+}
+
+if (document.querySelector("cart-drawer")) {
+  const view_cart_element = `
+  <a href="/cart" class="button button--primary view-cart">
+    View Cart
+  </a>
+  `;
+
+  document.querySelector("cart-drawer .drawer__footer .cart__ctas").insertAdjacentHTML("beforeend", view_cart_element);
 }
